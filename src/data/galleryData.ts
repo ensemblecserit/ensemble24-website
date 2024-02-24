d42d32e70f154037576a37f1fa3b5c5d965edcab
Alvin Varghese <alvin19official@gmail.com>
2024-02-24 23:01:10 +0530

    feat: add data of previous fest

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -1,16 +1,20 @@
 import { Gallery, Photo } from "./galleryTypes";
 
-const Logolaunch = [
-  new Photo("1.JPG", "65sd"),
-  new Photo("2.JPG", "65sd"),
-  new Photo("3.JPG", "65sd"),
-  new Photo("4.JPG", "65sd"),
+const PrevFest = [
+  new Photo("1.webp", "ENSEMBLE1"),
+  new Photo("2.webp", "ENSEMBLE2"),
+  new Photo("3.webp", "ENSEMBLE3"),
+  new Photo("4.webp", "ENSEMBLE4"),
 ];
 
 //
 // GALLERY ARRAY
 //
+// shortcuts
+const ll = "logo-launch";
+const pe = "pre-events";
+const r = "rebound";
+const g = "googly";
+const pf = "previous-fest";
 
-export const Galleries = [
-  new Gallery("logo-launch", "Logo Launch", Logolaunch),
-];
+export const Galleries = [new Gallery(pf, "Previous Fest Gallery", PrevFest)];
