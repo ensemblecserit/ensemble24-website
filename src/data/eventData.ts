e4aef1d825843ae140243f06f102862590208245
Alvin Varghese <alvin19official@gmail.com>
2024-02-26 15:33:00 +0530

    feat: ensemble

--- src/data/eventData.ts
+++ src/data/eventData.ts
@@ -8,26 +8,47 @@ const PreEvents = [
   new PostType(
     101,
     pe,
+    "rebound.jpeg",
+    "https://www.instagram.com/rebound.cse?igsh=MTI0ZHlzbnA2M3hwbg==",
+    false
+  ),
+  new PostType(
+    102,
+    pe,
+    "shootout.jpeg",
+    "https://www.instagram.com/p/C3plI4aS2QY/?igsh=anh4bnlub3dnZXM4",
+    false
+  ),
+  new PostType(
+    103,
+    pe,
+    "googly.jpeg",
+    "https://www.instagram.com/p/C3aYVbSynfI/?igsh=MW9seGp6aW1jdTV0aQ==",
+    false
+  ),
+  new PostType(
+    104,
+    pe,
     "valorant.jpeg",
     "https://docs.google.com/forms/d/e/1FAIpQLSdZsmkZRa0LUcD9MRv_HaEAAGSLBxqCaLeHTD8vIMDhoe3bAQ/closedform",
     false
   ),
   new PostType(
-    102,
+    105,
     pe,
     "trollmaking.jpeg",
     "https://docs.google.com/forms/d/e/1FAIpQLSfPjlSw8dlcAMvfzqm4NV0rN2Mjg6ZImgxeEab577h30Qb7-w/viewform",
     false
   ),
   new PostType(
-    103,
+    106,
     pe,
     "gitgithub.jpeg",
     "https://docs.google.com/forms/d/e/1FAIpQLSdsunfUasmWGeH02IEEmauCvsSSiga6ohjPR2mkDfVUZ9UWQw/viewform",
     false
   ),
   new PostType(
-    104,
+    107,
     pe,
     "efootball.jpeg",
     "https://docs.google.com/forms/d/e/1FAIpQLScM-i0EXnaTQ3yrLvbmOrLVpH-Dv8Mb59BpCwG5NPLA_GIMWA/viewform",
