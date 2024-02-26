90b344ded15ca121634c2693959e1d32dabd2170
Alvin Varghese <alvin19official@gmail.com>
2024-02-26 15:20:35 +0530

    feat: viewing saved posts

--- src/modules/Events/EventsLink/Events.tsx
+++ src/modules/Events/EventsLink/Events.tsx
@@ -1,9 +1,16 @@
 import Section from "@/components/Section";
+import SavedEvents from "./SavedEvents";
+import styles from "../events.module.css";
 
 const Events = () => {
   return (
     <Section id="events" tag="Events">
-      Events
+      <div className={styles.events_section}>
+        <div>Events listing</div>
+        <div>
+          <SavedEvents />
+        </div>
+      </div>
     </Section>
   );
 };
---
+++ src/modules/Events/EventsLink/SavedEvents.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { getAllPostsFromLocalStorage } from "@/lib/postSave";
+import { useEffect, useState } from "react";
+import EventsGroup from "../EventsGroup";
+
+const SavedEvents = () => {
+  const [posts, setPosts] = useState({});
+  useEffect(() => {
+    setPosts(getAllPostsFromLocalStorage());
+  }, []);
+  if (Object.keys(posts).length > 0)
+    return (
+      <EventsGroup
+        events={Object.values(posts)}
+        section="saved-events"
+        end={true}
+      />
+    );
+  else return <></>;
+};
+
+export default SavedEvents;
