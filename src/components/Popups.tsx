0c4096366251007b085f76096b9090ea74f9c0b6
fbn776 <fbn776@gmail.com>
2024-02-27 23:48:00 +0530

    feat: add popup hint

--- src/components/Popups.tsx
+++ src/components/Popups.tsx
@@ -3,126 +3,142 @@
 import cn from "@/lib/cn";
 import { Lightbulb, Music, PauseOctagon, StepForward } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Popups = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [first, setFirst] = useState(true);
-  const songs = ["audio/nebulakal.mp3"];
+	const [isOpen, setIsOpen] = useState(false);
+	const [first, setFirst] = useState(true);
+	const songs = ["audio/nebulakal.mp3"];
 
-  // dealing with popup
-  useEffect(() => {
-    if (!first) return;
-    const timeout = setTimeout(() => {
-      if (first) {
-        setIsOpen(true);
-        setFirst(false);
-      }
-    }, 1000);
+	// dealing with popup
+	useEffect(() => {
+		if (!first) return;
+		const timeout = setTimeout(() => {
+			if (first) {
+				setIsOpen(true);
+				setFirst(false);
+			}
+		}, 1000);
 
-    return () => clearTimeout(timeout);
-  }, [first]);
-  const handleYes = () => {
-    toggle();
-    setIsOpen(false);
-  };
-  const handleNo = () => {
-    setIsOpen(false);
-  };
+		return () => clearTimeout(timeout);
+	}, [first]);
+	const handleYes = () => {
+		toggle();
+		setIsOpen(false);
+	};
+	const handleNo = () => {
+		setIsOpen(false);
+	};
 
-  // setting songs
-  // State variables for managing audio playback
-  const [audio, setAudio] = useState<HTMLAudioElement>(); // Holds the current audio element
-  const [isPlaying, setIsPlaying] = useState<boolean>(false); // Indicates whether audio is playing
-  const [current, setCurrent] = useState<number>(-2); // Stores the current song index (-2 for initial state)
+  /**Adds a className to body tag on hint onClick and removes it after a set time */
+	const handleHints: () => void = useCallback(() => {
+		const hintCls = "hint-active";
+		let hasHint = document.body.classList.contains(hintCls);
+		let timeout;
+		if (!hasHint) {
+			document.body.classList.toggle(hintCls);
+			timeout = setTimeout(() => {
+				document.body.classList.toggle(hintCls);
+			}, 3000);
+		} else {
+			clearTimeout(timeout);
+		}
+	}, []);
 
-  // Function to handle loading and starting a new song
-  const setSong = async () => {
-    audio?.pause(); // Pause the current audio if any
-    if (isPlaying) setIsPlaying(false); // Update playing state
+	// setting songs
+	// State variables for managing audio playback
+	const [audio, setAudio] = useState<HTMLAudioElement>(); // Holds the current audio element
+	const [isPlaying, setIsPlaying] = useState<boolean>(false); // Indicates whether audio is playing
+	const [current, setCurrent] = useState<number>(-2); // Stores the current song index (-2 for initial state)
 
-    // Determining the next song index
-    let temp;
-    if (current === -2) temp = -1; // If initial state, set to -1 for first song
-    else temp = current === songs.length - 1 ? -1 : current; // If last song, loop back to -1
+	// Function to handle loading and starting a new song
+	const setSong = async () => {
+		audio?.pause(); // Pause the current audio if any
+		if (isPlaying) setIsPlaying(false); // Update playing state
 
-    let aud = new Audio(songs[temp + 1]); // Create a new Audio object for the next song
-    if (current !== -2) {
-      // If not initial state, Set playing state & start audio
-      setIsPlaying(true);
-      await aud.play();
-    }
+		// Determining the next song index
+		let temp;
+		if (current === -2) temp = -1; // If initial state, set to -1 for first song
+		else temp = current === songs.length - 1 ? -1 : current; // If last song, loop back to -1
 
-    setCurrent(temp + 1); // Update the current song index
-    setAudio(aud); // Set the new audio element in state
-  };
+		let aud = new Audio(songs[temp + 1]); // Create a new Audio object for the next song
+		if (current !== -2) {
+			// If not initial state, Set playing state & start audio
+			setIsPlaying(true);
+			await aud.play();
+		}
 
-  // Initial effect for playing the first song
-  // If initial state, load and play the first song
-  useEffect(() => {
-    if (current === -2) setSong();
-  }, []);
+		setCurrent(temp + 1); // Update the current song index
+		setAudio(aud); // Set the new audio element in state
+	};
 
-  // Effect for handling audio play/pause based on isPlaying state
-  // just using the state to apply pause and play functions
-  useEffect(() => {
-    const stopStart = async () => {
-      if (audio) {
-        if (isPlaying) await audio?.play();
-        else audio.pause();
-      }
-    };
+	// Initial effect for playing the first song
+	// If initial state, load and play the first song
+	useEffect(() => {
+		if (current === -2) setSong();
+	}, []);
 
-    stopStart();
-  }, [isPlaying]);
+	// Effect for handling audio play/pause based on isPlaying state
+	// just using the state to apply pause and play functions
+	useEffect(() => {
+		const stopStart = async () => {
+			if (audio) {
+				if (isPlaying) await audio?.play();
+				else audio.pause();
+			}
+		};
 
-  // Function to toggle play/pause
-  const toggle = () => audio && setIsPlaying(!isPlaying);
+		stopStart();
+	}, [isPlaying]);
 
-  // Function to handle moving to the next song if is not playing play first.
-  const handleNext = () => {
-    if (!isPlaying) toggle();
-    else setSong();
-  };
+	// Function to toggle play/pause
+	const toggle = () => audio && setIsPlaying(!isPlaying);
 
-  let size = 25;
-  return (
-    <>
-      <div className="logo_for_mobile">
-        <div>Ensemble'24 | </div>
-        <Image
-          src="/ensemble-white.webp"
-          height={20}
-          width={18}
-          alt="Ensemble '24"
-        />
-      </div>
+	// Function to handle moving to the next song if is not playing play first.
+	const handleNext = () => {
+		if (!isPlaying) toggle();
+		else setSong();
+	};
 
-      <div className={cn("popup", isOpen ? "open" : "close")}>
-        <div className="popup-content">
-          <p>Would you like to listen to some music?</p>
-          <button onClick={handleYes}>Yes</button>
-          <button onClick={handleNo}>No</button>
-        </div>
-      </div>
+	let size = 25;
+	return (
+		<>
+			<div className="logo_for_mobile">
+				<div>Ensemble'24 | </div>
+				<Image
+					src="/ensemble-white.webp"
+					height={20}
+					width={18}
+					alt="Ensemble '24"
+				/>
+			</div>
 
-      <div className="panel">
-        <button className="btn" onClick={toggle} style={{ display: "none" }}>
-          <Lightbulb size={size} />
-        </button>
-        {!first && !isOpen && (
-          <>
-            <button className="btn" onClick={toggle}>
-              {isPlaying ? <PauseOctagon size={size} /> : <Music size={size} />}
-            </button>
-            <button className="btn" onClick={handleNext}>
-              <StepForward size={size} />
-            </button>
-          </>
-        )}
-      </div>
-    </>
-  );
+			<div className={cn("popup", isOpen ? "open" : "close")}>
+				<div className="popup-content">
+					<p>Would you like to listen to some music?</p>
+					<button onClick={handleYes}>Yes</button>
+					<button onClick={handleNo}>No</button>
+				</div>
+			</div>
+
+			<div className="panel">
+				<button className="btn" onClick={handleHints}>
+					<Lightbulb size={size} />
+				</button>
+
+				{!first && !isOpen && (
+					<>
+						<button className="btn" onClick={toggle}>
+							{isPlaying ? <PauseOctagon size={size} /> : <Music size={size} />}
+						</button>
+						<button className="btn" onClick={handleNext}>
+							<StepForward size={size} />
+						</button>
+					</>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default Popups;
--- src/modules/Navbar/Navbar.tsx
+++ src/modules/Navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import styles from "./navbar.module.css";
 import { Hand, HomeIcon, Lightbulb, Mail, TicketPlus, Users } from "lucide-react";
 import Image from "next/image";
 import cn from "@/lib/cn";
+import About from '@/modules/About/About';
 
 const Navbar = () => {
   const container = useRef<HTMLDivElement>(null);
@@ -30,13 +31,16 @@ const Navbar = () => {
   return (
 		<div ref={container}>
 			<nav className={cn(styles.nav, "nav_animate")} draggable={false}>
-				<Link href="/" aria-label="Home">
+				<Link href="/" aria-label="Home" className="popup-cont">
+					<span className="popup-element">Home</span>
 					<HomeIcon />
 				</Link>
-				<Link href="#about" aria-label="About">
+				<Link href="#about" aria-label="About" className="popup-cont">
+					<span className="popup-element">About</span>
 					<Users />
 				</Link>
-				<Link href="/glimpses" aria-label="Glimpses">
+				<Link href="/glimpses" aria-label="Glimpses" className="popup-cont">
+					<span className="popup-element">Glimpses</span>
 					<Image
 						className={styles.logo}
 						height={27}
@@ -47,22 +51,15 @@ const Navbar = () => {
 						priority
 					/>
 				</Link>
-				<Link href="/events" aria-label="Events">
+				<Link href="/events" aria-label="Events" className="popup-cont">
+					<span className="popup-element">Events</span>
 					<TicketPlus />
 				</Link>
-				<Link href="#contact" aria-label="Contact">
+				<Link href="#contact" aria-label="Contact" className="popup-cont">
+					<span className="popup-element">Contact</span>
 					<Mail />
 				</Link>
 			</nav>
-
-			<div className={styles.hints}>
-				<span>
-					<Hand />
-				</span>
-				<button aria-label="Hint" >
-					<Lightbulb />
-				</button>
-			</div>
 		</div>
 	);
 };
