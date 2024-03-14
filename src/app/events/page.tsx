479fd3502ce6b44fec89bd5f366519e278e4889d
Alvin Varghese <alvin19official@gmail.com>
2024-03-14 21:09:09 +0530

    feat: footer in events, glimpses pages

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,13 +1,17 @@
 import Heading from "@/components/Heading";
 import ProvidePadding from "@/components/ProvidePadding";
 import EventsPage from "@/modules/Events/EventsPage";
+import Footer from "@/modules/Footer/Footer";
 
 const Page = () => {
   return (
-    <ProvidePadding>
-      <Heading headingLine1="Events in" headingLine2="ENSEMBLE" />
-      <EventsPage />
-    </ProvidePadding>
+    <>
+      <ProvidePadding>
+        <Heading headingLine1="Events in" headingLine2="ENSEMBLE" />
+        <EventsPage />
+      </ProvidePadding>
+      <Footer />
+    </>
   );
 };
 
--- src/app/glimpses/page.tsx
+++ src/app/glimpses/page.tsx
@@ -3,16 +3,20 @@ import GlimpsesPage from "@/modules/Gallery/GlimpsesPage";
 import { GalleryProvider } from "@/modules/Gallery/GalleryContext";
 import ImageModal from "@/modules/Gallery/Glimpses/ImageModal";
 import Heading from "@/components/Heading";
+import Footer from "@/modules/Footer/Footer";
 
 const page = () => {
   return (
-    <ProvidePadding>
-      <Heading headingLine1="Glimpses of" headingLine2="ENSEMBLE" />
-      <GalleryProvider>
-        <ImageModal />
-        <GlimpsesPage />
-      </GalleryProvider>
-    </ProvidePadding>
+    <>
+      <ProvidePadding>
+        <Heading headingLine1="Glimpses of" headingLine2="ENSEMBLE" />
+        <GalleryProvider>
+          <ImageModal />
+          <GlimpsesPage />
+        </GalleryProvider>
+      </ProvidePadding>
+      <Footer />
+    </>
   );
 };
 
